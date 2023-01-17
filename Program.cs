7b1fa3e0912b0ac3041d297d5eba8311da146e55
ErikSulzbach <e.sulzbach22@gmail.com>
2023-01-17 13:37:54 +0100

    Choice in console for user

--- Program.cs
+++ Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        // test
+        while (true)
+        {
+            Console.WriteLine("1. List all To-Dos for today.\n" +
+                "2. List all To-Dos that you have.\n" +
+                "3. Add new To-Do.\n" +
+                "4. Exit.\n");
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+
+            }
+            else if (choice == "2")
+            {
+            
+            }
+            else if (choice == "3")
+            {
+
+            }
+            else if (choice == "4")
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("Please check your input. It is only possible to enter Numbers from 1 to 4");
+                continue;
+            }
+        }
+        
     }
 }
